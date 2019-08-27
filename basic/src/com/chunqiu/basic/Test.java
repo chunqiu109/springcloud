26ab34640cf24f7d2bda509fb71128cb4e40b7ad
Administrator <chunqiu.wang@symbio.com>
2019-08-27 17:36:18 +0800

    冒泡排序

--- basic/src/com/chunqiu/basic/Test.java
+++ basic/src/com/chunqiu/basic/Test.java
@@ -3,8 +3,7 @@ package com.chunqiu.basic;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
---
+++ basic/src/com/chunqiu/basic/sort/Sort.java
@@ -0,0 +1,43 @@
+package com.chunqiu.basic.sort;
+
+/**
+ * 
+* @ClassName: Sort
+* @Description: 排序算法测试类
+* @author Administrator
+* @date 2019年8月27日
+*
+ */
+public class Sort {
+
+	public static void main(String[] args) {
+		int [] arr  = {45,8,7,24,18};
+		popSort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		
+	}
+	
+	/**
+	 * 
+	* @Title: popSort
+	* @Description: 冒泡排序
+	* @param     参数
+	* @return void    返回类型
+	* @throws
+	 */
+	public static void popSort(int [] arr) {
+		
+		for (int i = 0; i < arr.length - 1; i++) {//声明比较的轮数
+			for (int j = 0; j < arr.length - 1 - i; j++) {
+				if (arr[j] > arr[j +1]) {
+					int tmp = arr [j + 1];
+					arr[j + 1]= arr [j];
+					arr[j]= tmp;
+				}
+			}
+		}
+		
+	}
+}
